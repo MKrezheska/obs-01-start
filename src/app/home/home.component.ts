9406d763cdb8a93c87b50e25fb1ccf37b7ec9304
Magdalena Krezheska <Magdalena.Krezheska@netcetera.com>
2020-09-07 13:48:50 +0200

    Operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
@@ -20,14 +21,31 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        } // it is dead after this, no need for unsubscribe
         count++;
         // observer.error();
         // observer.complete();
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+
+
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    } ), map((data: number) => {
+      return 'Round ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
     });
   }
 
